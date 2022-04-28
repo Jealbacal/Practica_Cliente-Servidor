0ec73a900afd5ec7561c39bfc3386c13ed0af03f
Jeal <jesusagustiniano@gmail.com>
2022-04-28 12:39:37 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -8,14 +8,27 @@ import java.net.Socket;
 
 public class Cliente {
     private static final int socket=999;
-    private static int op;
+
 
     public static void main(String args[]) throws IOException {
         Socket s = new Socket("localhost",socket);
-        while (true) {
+        int op;
+        op=menu();
+        while (op !=5) {
 
 
-            (new OyenteServidor(s,op)).start();
         }
+        new OyenteServidor(s).start();
+
+    }
+
+    public static int menu(){
+
+        //todo
+        System.out.println(" --------------------------------------");
+        System.out.println("|                MENU                  |");
+        System.out.println(" --------------------------------------");
+
+        return 0;
     }
 }
--- src/clientes/OyenteServidor.java
+++ src/clientes/OyenteServidor.java
@@ -1,23 +1,70 @@
 package clientes;
 
+import mensajes.Mensaje;
+import mensajes.MensajeConexion;
+import mensajes.MensajeConfirmacionConexion;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class OyenteServidor extends Thread{
+    private Mensaje mensaje;
     private Socket s;
-    private int op;
 
 
-    public OyenteServidor(Socket s,int op){
+    public OyenteServidor(Socket s){
         this.s=s;
-        this.op=op;
+
 
     }
 
     @Override
     public void run(){
 
-        while(true){
-            
+         try {
+            ObjectInputStream fin = new ObjectInputStream(s.getInputStream());
+
+            while (true) {
+                //todo
+
+
+                mensaje = (Mensaje) fin.readObject();
+
+                switch (mensaje.getTipo()) {
+
+                    case 1:
+                        //mensaje de confirmacion de conexion
+
+
+                    case 2:
+                        //mensaje de confirmacion.lista de usuario
+
+                        break;
+
+                    case 3:
+                        //mensaje de emitir fichero
+                        break;
+
+                    case 4:
+                        //mensaje de preparado S->C
+                        break;
+
+                    case 5:
+                        //mensaje de confirmacion cerrar conex
+                        break;
+
+                    case 6:
+                        //mensaje de confirmacion
+                        break;
+
+
+                }
+            }
+        }
+        catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
 
     }
